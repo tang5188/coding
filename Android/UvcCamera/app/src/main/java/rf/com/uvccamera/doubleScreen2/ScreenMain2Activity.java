8da0f06f4666460be606e67335a837d160bf78bd
tangpj <tangpj@samples.cn>
2018-12-19 10:03:45 +0800

    1.双屏测试分辨率方法修改 2.主屏160dpi，1280*800；副屏160dpi，1920*1080。增加相应分辨率图片，加以验证。

--- Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenMain2Activity.java
+++ Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenMain2Activity.java
@@ -1,5 +1,6 @@
 package rf.com.uvccamera.doubleScreen2;
 
+import android.app.Activity;
 import android.app.Presentation;
 import android.content.Context;
 import android.hardware.display.DisplayManager;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 
 import rf.com.uvccamera.R;
@@ -15,7 +17,7 @@ import rf.com.uvccamera.R;
 /**
  * 参考文档：http://www.cnblogs.com/IWings/p/6094708.html
  */
-public class ScreenMain2Activity extends AppCompatActivity {
+public class ScreenMain2Activity extends Activity {
 
     Presentation mPresentation;
 
@@ -24,18 +26,32 @@ public class ScreenMain2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_main2);
 
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        Log.e("分辨率", "Density is " + displayMetrics.density + " densityDpi is " + displayMetrics.densityDpi + " height: " + displayMetrics.heightPixels +
-                " width: " + displayMetrics.widthPixels);
-
         DisplayManager mDisplayManager;// 屏幕管理类
         mDisplayManager = (DisplayManager) this.getSystemService(Context.DISPLAY_SERVICE);
         Display[] displays = mDisplayManager.getDisplays();
-
         if (mPresentation == null) {
             mPresentation = new ScreenSecond2Presentation(this, displays[displays.length - 1]);// displays[1]是副屏
             mPresentation.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
             mPresentation.show();
         }
+
+        //设置全屏
+        setFullScreen();
+        //分辨率显示
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+        Log.e("分辨率1", "Density is " + metrics.density + " densityDpi is " + metrics.densityDpi + " height: " + metrics.heightPixels +
+                " width: " + metrics.widthPixels);
+    }
+
+    private void setFullScreen() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
--- Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenSecond2Presentation.java
+++ Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenSecond2Presentation.java
@@ -3,6 +3,8 @@ package rf.com.uvccamera.doubleScreen2;
 import android.app.Presentation;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 
 import rf.com.uvccamera.R;
@@ -16,5 +18,10 @@ public class ScreenSecond2Presentation extends Presentation {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.presentation_screen_second2);
+
+        //分辨率显示
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        Log.e("分辨率2", "Density is " + metrics.density + " densityDpi is " + metrics.densityDpi + " height: " + metrics.heightPixels +
+                " width: " + metrics.widthPixels);
     }
 }
