acb99599bc8d5f5495b2938b06a524c77de726f0
tangpj <tangpj@samples.cn>
2018-12-19 19:23:03 +0800

    主副屏轮播效果实现

--- Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenMain2Activity.java
+++ Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenMain2Activity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Presentation;
 import android.content.Context;
 import android.hardware.display.DisplayManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -12,14 +11,24 @@ import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
 import rf.com.uvccamera.R;
 
 /**
  * 参考文档：http://www.cnblogs.com/IWings/p/6094708.html
  */
-public class ScreenMain2Activity extends Activity {
+public class ScreenMain2Activity extends Activity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
-    Presentation mPresentation;
+    private Presentation mPresentation;
+
+    private SliderLayout mSlider;
+    private PagerIndicator mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,46 @@ public class ScreenMain2Activity extends Activity {
         getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
         Log.e("分辨率1", "Density is " + metrics.density + " densityDpi is " + metrics.densityDpi + " height: " + metrics.heightPixels +
                 " width: " + metrics.widthPixels);
+
+        mSlider = (SliderLayout) findViewById(R.id.home_slider_ad);
+        mIndicator = (PagerIndicator) findViewById(R.id.home_indicator_ad);
+        this.initSlider();
+    }
+
+    @Override
+    protected void onStop() {
+        mSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    private void initSlider() {
+
+        TextSliderView textSliderView1 = new TextSliderView(ScreenMain2Activity.this);
+        textSliderView1.image(R.drawable.screen001)
+                .description("主屏照片1")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView1);
+
+        TextSliderView textSliderView2 = new TextSliderView(ScreenMain2Activity.this);
+        textSliderView2.image(R.drawable.screen002)
+                .description("主屏照片2")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView2);
+
+        TextSliderView textSliderView3 = new TextSliderView(ScreenMain2Activity.this);
+        textSliderView3.image(R.drawable.screen003)
+                .description("主屏照片3")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView3);
+
+        mSlider.setCustomIndicator(mIndicator);
+        mSlider.setCustomAnimation(new DescriptionAnimation());
+        mSlider.setPresetTransformer(SliderLayout.Transformer.ZoomOut);
+        mSlider.setDuration(7000);
+        mSlider.addOnPageChangeListener(this);
     }
 
     private void setFullScreen() {
@@ -54,4 +103,26 @@ public class ScreenMain2Activity extends Activity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
+
+    // region 轮播回调
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    // endregion
 }
--- Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenSecond2Presentation.java
+++ Android/UvcCamera/app/src/main/java/rf/com/uvccamera/doubleScreen2/ScreenSecond2Presentation.java
@@ -7,9 +7,20 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
 import rf.com.uvccamera.R;
 
-public class ScreenSecond2Presentation extends Presentation {
+public class ScreenSecond2Presentation extends Presentation implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+
+    private SliderLayout mSlider;
+    private PagerIndicator mIndicator;
+
     public ScreenSecond2Presentation(Context outerContext, Display display) {
         super(outerContext, display);
     }
@@ -23,5 +34,67 @@ public class ScreenSecond2Presentation extends Presentation {
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         Log.e("分辨率2", "Density is " + metrics.density + " densityDpi is " + metrics.densityDpi + " height: " + metrics.heightPixels +
                 " width: " + metrics.widthPixels);
+
+        mSlider = (SliderLayout) findViewById(R.id.home_slider_ad);
+        mIndicator = (PagerIndicator) findViewById(R.id.home_indicator_ad);
+        this.initSlider();
     }
+
+    @Override
+    protected void onStop() {
+        mSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    private void initSlider() {
+
+        TextSliderView textSliderView1 = new TextSliderView(getContext());
+        textSliderView1.image(R.drawable.screen101)
+                .description("副屏照片1")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView1);
+
+        TextSliderView textSliderView2 = new TextSliderView(getContext());
+        textSliderView2.image(R.drawable.screen102)
+                .description("副屏照片2")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView2);
+
+        TextSliderView textSliderView3 = new TextSliderView(getContext());
+        textSliderView3.image(R.drawable.screen103)
+                .description("副屏照片3")
+                .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                .setOnSliderClickListener(this);
+        mSlider.addSlider(textSliderView3);
+
+        mSlider.setCustomIndicator(mIndicator);
+        mSlider.setCustomAnimation(new DescriptionAnimation());
+        mSlider.setPresetTransformer(SliderLayout.Transformer.CubeIn);
+        mSlider.setDuration(8000);
+        mSlider.addOnPageChangeListener(this);
+    }
+
+    // region 轮播回调
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    // endregion
 }
